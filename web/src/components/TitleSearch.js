7d4f1efd4385a93706362b6a310efe1c2c403baf
hhnisenbaum <hhnisenbaum@gmail.com>
2021-07-24 14:51:45 +1200

    Add label to input for accessibility

--- web/src/components/TitleSearch.js
+++ web/src/components/TitleSearch.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import { withRouter } from "react-router-dom";
-import { Form, Input, Button } from "reactstrap";
+import { Form, Input, Button, Label } from "reactstrap";
 
 class TitleSearch extends Component {
     constructor(props) {
@@ -21,6 +21,13 @@ class TitleSearch extends Component {
       return (
         <div style={{display: "inline-block"}}>
           <Form inline onSubmit={this.handleSubmit}>
+            {/* 
+              Add label for accessibility 
+              src: https://reach.tech/visually-hidden/
+            */}
+            <span style={ visuallyHiddenCss }>
+              <Label htmlFor="title">Search title</Label>
+            </span>
             <Input id="title" type="number" value={this.state.value} onChange={this.handleChange} placeholder="Enter a title number" />
             &nbsp;
             <Button color="primary" type="submit" value="Submit">Go</Button>
@@ -31,3 +38,16 @@ class TitleSearch extends Component {
   }
 
   export default withRouter(TitleSearch);
+
+  const visuallyHiddenCss = {
+    border: 0,
+    clip: "rect(0 0 0 0)",
+    height: "1px",
+    margin: "-1px",
+    overflow: "hidden",
+    padding: 0,
+    position: "absolute",
+    width: "1px",
+    whiteSpace: "nowrap",
+    wordWrap: "normal",
+  }
